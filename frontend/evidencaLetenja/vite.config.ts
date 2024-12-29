a193d549768d1ccaa57345948d813987ac1f00ac
DavidBalazic <davidbalazic31@hotmail.com>
2024-12-29 13:25:08 +0100

    fix: updated vite config and commented out check gate quality

--- frontend/evidencaLetenja/vite.config.ts
+++ frontend/evidencaLetenja/vite.config.ts
@@ -12,5 +12,6 @@ export default defineConfig({
 	  globals: true,
     environment: 'jsdom',
     setupFiles: ['./vitest-setup.js'],
+    include: ['src/**/*.{ts,tsx}', 'tests/**/*.{ts,tsx}'],
   },
 })
